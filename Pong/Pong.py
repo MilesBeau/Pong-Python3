cd6b16f28ccaf619a523b500f39e2b0f032c57f6
MilesBeau <milesb3au@gmail.com>
2024-10-13 13:09:45 +0100

    created left paddle

--- Pong/Pong.py
+++ Pong/Pong.py
@@ -1,9 +1,34 @@
 # Simple Pong made in Python3
 
+# Importing turtle and setting up wn 
 import turtle
 
-wn = turtle.Sceen()
+wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+# Game Objects
+
+# Paddle 1
+
+paddle_one = turtle.Turtle()
+paddle_one.speed(0)
+paddle_one.shape("square")
+paddle_one.color("white")
+paddle_one.shapesize(stretch_wid=5, stretch_len=1)
+paddle_one.penup()
+paddle_one.goto(-350, 0)
+
+# Paddle 2
+
+# Ball
+
+# Main Game Loop
+while True:
+    wn.update()
+    
+
+
+
