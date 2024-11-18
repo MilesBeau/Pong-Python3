33ca131ec607748bd39c31cc57205b8418d81a0e
MilesBeau <milesb3au@gmail.com>
2024-11-18 17:34:18 +0000

    added movement to both paddles

--- Pong/Pong.py
+++ Pong/Pong.py
@@ -12,7 +12,6 @@ wn.tracer(0)
 # Game Objects
 
 # Paddle 1
-
 paddle_one = turtle.Turtle()
 paddle_one.speed(0)
 paddle_one.shape("square")
@@ -22,8 +21,52 @@ paddle_one.penup()
 paddle_one.goto(-350, 0)
 
 # Paddle 2
+paddle_two = turtle.Turtle()
+paddle_two.speed(0)
+paddle_two.shape("square")
+paddle_two.color("white")
+paddle_two.shapesize(stretch_wid=5, stretch_len=1)
+paddle_two.penup()
+paddle_two.goto(350, 0)
+
 
 # Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+# Functions
+def paddle_one_up():
+    y = paddle_one.ycor()
+    y += 20
+    paddle_one.sety(y)
+    
+def paddle_one_down():
+    y = paddle_one.ycor()
+    y -= 20
+    paddle_one.sety(y)
+    
+def paddle_two_up():
+    y = paddle_two.ycor()
+    y += 20
+    paddle_two.sety(y)
+    
+def paddle_two_down():
+    y = paddle_two.ycor()
+    y -= 20
+    paddle_two.sety(y)
+    
+# Keyboard Binding
+wn.listen()
+wn.onkeypress(paddle_one_up, "w")
+wn.onkeypress(paddle_one_down, "s")
+wn.onkeypress(paddle_two_up, "Up")
+wn.onkeypress(paddle_two_down, "Down")
+
+
 
 # Main Game Loop
 while True:
